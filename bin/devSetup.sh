35f8487841b26c6967d5a0371e758d39c9b12972
naterfute <naterfute@pyro.host>
2026-07-01 21:49:15 -0700

    change default email

--- bin/devSetup.sh
+++ bin/devSetup.sh
@@ -2,7 +2,7 @@
 
 lerd setup -a
 ./bin/s3Setup.sh .env
-lerd artisan p:user:make -n --email dev@pyro.host --username dev --name-first Developer --name-last User --password dev --admin  > /dev/null
+lerd artisan p:user:make -n --email dev@hydrodactyl.dev --username dev --name-first Developer --name-last User --password dev --admin  > /dev/null
 lerd artisan p:location:make -n --short local --long Local > /dev/null
 lerd artisan p:node:make -n --name local --description "Development Node" --locationId 1 --fqdn localhost --internal-fqdn host.containers.internal --public 1 --scheme http --proxy 1 --daemonListeningPort 8080 --maxMemory 1024 --maxDisk 10240 --overallocateMemory 0 --overallocateDisk 0 --daemonType wings --backupDisk s3 --bucket 1 > /dev/null
 
